2217a6468d9d23f4561f12bb5b6b79969224073d
Starinin Andrey <starininandrey@gmail.com>
2022-02-17 20:40:49 +0300

    Init

---
+++ SQL/create_tables.sql
@@ -0,0 +1,25 @@
+﻿CREATE TABLE tab_trademarks
+(
+    id   INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    name TEXT    NOT NULL
+);
+
+CREATE TABLE tab_phones
+(
+    id           INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    id_trademark INTEGER NOT NULL,
+    model        TEXT    NOT NULL,
+    FOREIGN KEY (id_trademark) REFERENCES tab_trademarks(id) 
+        ON DELETE NO ACTION 
+        ON UPDATE NO ACTION 
+);
+
+CREATE TABLE tab_prices
+(
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    id_phone INTEGER NOT NULL,
+    price REAL NOT NULL,
+    FOREIGN KEY (id_phone) REFERENCES tab_phones(id)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION                     
+);
